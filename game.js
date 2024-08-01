3945bd1a75c9806e1134f6cecb294ff32ce16a0f
Sumit-2005 <sumitbhagat9266@gmail.com>
2024-08-01 21:21:05 +0530

    project restart:

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
-const base_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json"
+const base_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
 
 const dropdowns = document.querySelectorAll("select");
-console.dir(dropdowns)
+
+
 
